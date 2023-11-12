11f947e8a022b5a18d80da03659488243ef57cf2
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-12 14:52:19 +0100

    feat(guard): implemented the guard function and make it run on a different thread

--- src/components/guard.py
+++ src/components/guard.py
@@ -1,11 +1,70 @@
-import pykka
-
-class Guard(pykka.ThreadingActor):
-    def __init__(self, k_big: int, k: int, sys_mcl: float) :
+import time
+import threading
+from pykka import ThreadingActor
+class Guard(ThreadingActor):
+   
+    def __init__(self, k_big: int, k: int, sleep: int = 10):
         super().__init__()
         self.k_big = k_big
         self.k = k
-        self.sys_mcl = sys_mcl
-    
+        self.sleep = sleep
+        
+        self.running = True
+
+        self.inbound_workload = 0
+        self.system_mcl = self._get_system_mcl()
+
+        self.request_scaling = False
+
     def on_receive(self, message):
-        return "Hi, I'm a guard!"
+        print("Guard received: " + message)
+        return "Guard response"
+    
+    def on_start(self):
+        self.guard_thread = threading.Thread(target=self.guard)
+        self.guard_thread.start()
+    
+    def on_stop(self):
+        self._cleanup_actor()
+
+    def on_failure(self, exception_type, exception_value, traceback):
+        self._cleanup_actor()
+
+    def guard(self):
+        """
+        This method is executed in a separate thread.
+        Check the conditions of the system and eventually scale it.
+        """
+        while self.running:
+            self.inbound_workload = self._get_inbound_workload()
+            if(self.inbound_workload - (self.system_mcl - self.k_big) > self.k or
+               (self.system_mcl - self.k_big) - self.inbound_workload > self.k):
+                # Prevent multiple scaling requests
+                self.request_scaling = True
+                # TODO: send scaling request to the system
+                print("Scale up/down")
+            time.sleep(self.sleep)
+
+    def _get_system_mcl(self):
+        """
+        Return the actual system MCL
+        """
+        return 0
+
+    def _get_inbound_workload(self):
+        """
+        Return the inbound workload of the system, 
+        querying the external monitoring system.
+        """
+        return 0
+    
+    def _cleanup_actor(self): 
+        """
+        Stop the guard thread and join it.
+        """
+        if self.guard_thread.is_alive():
+            self.running = False
+            self.guard_thread.join()
+
+    
+    
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from components.guard import Guard
 
-# Instantiate the guard
-guard = Guard.start(1, 2, 3.0)
 
-answer = guard.ask("Hi, I'm a message!")
-print(answer) # Hi, I'm a guard!
+import time
 
-guard.stop()
+if __name__ == '__main__':
+    guard = Guard.start(1, 1, sleep=2)
+    time.sleep(3)
+    print("Sending message to Guard")
+    response = guard.ask("Hello Guard")
+    print(response)
 
