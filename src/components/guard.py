daba52d036828a35643f5b4c226b4fa43140d1c9
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-12 17:42:14 +0100

    test(guard): create tests for guard

--- src/components/guard.py
+++ src/components/guard.py
@@ -12,51 +12,62 @@ class Guard(ThreadingActor):
         self.running = True
 
         self.inbound_workload = 0
-        self.system_mcl = self._get_system_mcl()
+        self.system_mcl = self.get_system_mcl()
 
         self.request_scaling = False
 
-    def on_receive(self, message):
-        print("Guard received: " + message)
-        return "Guard response"
-    
-    def on_start(self):
+    def on_start(self) -> None:
         self.guard_thread = threading.Thread(target=self.guard)
         self.guard_thread.start()
     
-    def on_stop(self):
+    def on_stop(self) -> None:
         self._cleanup_actor()
 
-    def on_failure(self, exception_type, exception_value, traceback):
+    def on_failure(self, exception_type, exception_value, traceback) -> None:
         self._cleanup_actor()
-
-    def guard(self):
+    
+    def mark_scaling_as_done(self) -> None:
         """
-        This method is executed in a separate thread.
-        Check the conditions of the system and eventually scale it.
+        Mark the scaling request as done.
+        This method is called by the scaler when the scaling is completed.
         """
-        while self.running:
-            self.inbound_workload = self._get_inbound_workload()
-            if(self.inbound_workload - (self.system_mcl - self.k_big) > self.k or
-               (self.system_mcl - self.k_big) - self.inbound_workload > self.k):
-                # Prevent multiple scaling requests
-                self.request_scaling = True
-                # TODO: send scaling request to the system
-                print("Scale up/down")
-            time.sleep(self.sleep)
-
-    def _get_system_mcl(self):
+        self.request_scaling = False
+    
+    def get_system_mcl(self) -> int:
         """
         Return the actual system MCL
         """
         return 0
 
-    def _get_inbound_workload(self):
+    def get_inbound_workload(self) -> int:
         """
         Return the inbound workload of the system, 
         querying the external monitoring system.
         """
         return 0
+    
+    def should_scale(self) -> bool:
+        """
+        Check the conditions of the system and return True if it should scale.
+        """
+        print(self.inbound_workload, self.system_mcl, self.k_big, self.k)
+        return self.inbound_workload - (self.system_mcl - self.k_big) > self.k or \
+                (self.system_mcl - self.k_big) - self.inbound_workload > self.k
+
+    def guard(self) -> None:
+        """
+        This method is executed in a separate thread.
+        Check the conditions of the system and eventually scale it.
+        """
+        while self.running:
+            # Update variables
+            self.inbound_workload = self.get_inbound_workload()
+            self.system_mcl = self.get_system_mcl()
+            if self.should_scale :
+                # Prevent multiple scaling requests
+                self.request_scaling = True
+                # TODO: send scaling request to the system
+            time.sleep(self.sleep)
     
     def _cleanup_actor(self): 
         """
---
+++ test/components/test_guard.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+
+# Path to the src folder
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
+
+import pytest
+
+from components.guard import Guard
+
+@pytest.fixture()
+def standard_guard():
+    guard = Guard.start(k_big=2, k=5, sleep=2).proxy()
+    yield guard
+    guard.stop()
+
+def test_get_system_mcl(monkeypatch, standard_guard):
+    monkeypatch.setattr(standard_guard, "get_system_mcl", lambda: 60)
+    assert standard_guard.get_system_mcl().get() == 60
+
+def test_get_inbound_workload(monkeypatch, standard_guard):
+    monkeypatch.setattr(standard_guard, "get_inbound_workload", lambda: 60)
+    assert standard_guard.get_inbound_workload().get() == 60
+
+
+# def test_scaling_request(monkeypatch, standard_guard):
+#     monkeypatch.setattr(standard_guard, "get_system_mcl", lambda: 60)
+#     monkeypatch.setattr(standard_guard, "get_inbound_workload", lambda: 90)
+
+#     assert standard_guard.should_scale().get() == True
