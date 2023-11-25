21b4e87ceae1705894a34e185188fbf7033c9e4d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-25 23:29:49 +0100

    feature: add deploy_pod funciton

--- src/components/sys_scaler.py
+++ src/components/sys_scaler.py
@@ -1,4 +1,7 @@
+import yaml
+from kubernetes import client, config
 from components.configurator import Configurator
+from kubernetes.client.rest import ApiException
 
 class SysScaler:
     """
@@ -7,6 +10,9 @@ class SysScaler:
     def __init__(self, starting_mcl: int, configurator: Configurator) -> None:
         self.mcl = starting_mcl
         self.configurator = configurator
+    
+        config.load_incluster_config()
+        self.k8s_client = client.CoreV1Api()
 
     def get_mcl(self) -> int:
         """
@@ -36,6 +42,24 @@ class SysScaler:
 
         Arguments
         -----------
-        configuration_file -> the target mcl to reach 
+        configuration_file -> configuration to apply in the system
         """
+        
         pass
+
+    def deploy_pod(self, manifest_file_path) -> bool:
+        """
+        Deploy a pod in the cluster.
+
+        Arguments
+        -----------
+        manifest_file_path -> path to the manifest file to deploy
+        """
+        try:
+            with open(manifest_file_path, 'r') as manifest_file:
+                pod_manifest = yaml.safe_load(manifest_file)
+            api_response = self.k8s_client.create_namespaced_pod( body=pod_manifest, namespace="default")
+            print("Pod created. status='%s'" % str(api_response.status))
+            return True
+        except ApiException as e:
+            raise Exception(f"Error deploying pod: {e}")
