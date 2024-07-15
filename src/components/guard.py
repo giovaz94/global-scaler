8265b66b05c3102fdf034be75bc05215b10c2abe
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-15 11:33:50 +0200

    introduce predictions

--- src/components/guard.py
+++ src/components/guard.py
@@ -1,7 +1,6 @@
 import os
 import time
 import threading
-import requests
 from components.sys_scaler import SysScaler
 from prometheus_api_client import PrometheusConnect
 
@@ -11,10 +10,10 @@ class Guard:
     def __init__(
             self,
             scaler: SysScaler,
+            predictions: list[int],
             k_big: int,
             k: int,
             sleep: int = 1,
-            sampling_counter: int = 10 #not needed anymore 
     ):
         self.guard_thread = None
 
@@ -27,13 +26,13 @@ class Guard:
         self.request_scaling = False
         self.scaler = scaler
 
-        self.samplings = sampling_counter
-        self.__sampling_list = []
-
         prometheus_service_address = os.environ.get("PROMETHEUS_SERVICE_ADDRESS", "localhost")
         prometheus_service_port = os.environ.get("PROMETHEUS_SERVICE_PORT", "8080")
         prometheus_url = f"http://{prometheus_service_address}:{prometheus_service_port}"
         self.prometheus_instance = PrometheusConnect(url=prometheus_url)
+
+        self.proactiveness = True #change to an env variable
+        self.predictions = predictions
 
     def start(self) -> None:
         """
@@ -45,25 +44,6 @@ class Guard:
         """
         self.guard_thread = threading.Thread(target=self.guard)
         self.guard_thread.start()
-
-    def collect_sample(self) -> None:
-        """
-        Return the inbound workload of the system, 
-        querying the external monitoring system.
-        """
-        query = f"sum(increase(http_requests_total_parser[{self.sleep}s]))"
-        print(f"Querying: {query}", flush=True)
-        try:
-            data = self.prometheus_instance.custom_query(query)
-            metric_value = float(data[0]['value'][1])
-            if metric_value is not None and metric_value > 0:
-                self.__sampling_list.append(float(metric_value))
-                print(f"Sample collected: {self.__sampling_list}", flush=True)
-            else:
-                print(f"Value is {metric_value}", flush=True)
-                
-        except (requests.exceptions.RequestException, KeyError, IndexError) as e:
-            print("Error:", e, flush=True)
 
     def should_scale(self, inbound_workload, current_mcl) -> bool:
         """
@@ -80,18 +60,22 @@ class Guard:
         print("Monitoring the system...")
         res =  self.prometheus_instance.custom_query("http_requests_total_parser")
         init_val = float(res[0]['value'][1])
-        sl = self.sleep
+        sl = 1
+        iter = 0
         while self.running:
             time.sleep(sl)
             print("Checking the system...", flush=True)
-            inbound_workload = (tot-init_val)/10
-            print(f"Inbound workload: {inbound_workload}", flush=True)
+            target_workload = (tot-init_val)/sl
+            if iter > 0 and self.proactiveness:
+                target_workload = sum(self.predictions[iter-self.sleep:iter])/sl
+            print(f"Target workload: {target_workload}", flush=True)
             current_mcl = self.scaler.get_mcl()
-            if self.should_scale(inbound_workload, current_mcl):
-                self.scaler.process_request(inbound_workload)
+            if self.should_scale(target_workload, current_mcl):
+                self.scaler.process_request(target_workload)
             res =  self.prometheus_instance.custom_query("http_requests_total_parser")
             tot = float(res[0]['value'][1])
             if tot - init_val > 0:
                 init_val = tot
-                sl = 10
+                sl = self.sleep
+                iter += sl
             
--- src/main.py
+++ src/main.py
@@ -36,11 +36,35 @@ if __name__ == '__main__':
         [3, 6, 2, 2, 6, 6, 6],  # Increment 4
     ])
 
+    #worklod predictions
+    predictions = [
+                50, 62, 0, 75, 62, 40, 0, 27, 47, 92,
+                45, 37, 55, 60, 72, 7, 267, 512, 485, 522,
+                520, 522, 512, 540, 575, 570, 575, 632, 620, 630,
+                617, 610, 577, 600, 592, 555, 550, 570, 567, 562,
+                562, 562, 550, 537, 537, 552, 565, 580, 640, 665,
+                665, 737, 725, 722, 735, 725, 695, 732, 737, 732,
+                715, 690, 712, 707, 710, 695, 697, 730, 722, 727,
+                670, 572, 585, 580, 580, 575, 540, 525, 522, 520,
+                517, 525, 502, 507, 507, 507, 500, 500, 500, 487,
+                500, 470, 455, 427, 427, 420, 407, 400, 400, 392,
+                425, 430, 397, 367, 365, 337, 315, 325, 332, 322,
+                312, 315, 317, 315, 312, 317, 292, 280, 282, 272,
+                272, 275, 287, 305, 305, 297, 305, 317, 330, 320,
+                312, 325, 322, 320, 312, 327, 310, 300, 280, 262,
+                262, 257, 252, 252, 247, 260, 240, 230, 220, 230,
+                222, 232, 217, 217, 190, 190, 180, 175, 175, 175,
+                187, 115, 97, 82, 82, 65, 55, 35, 80, 77,
+                92, 140, 150, 37, 117, 82, 155, 147, 262, 395,
+                402, 437, 430, 447, 437, 452, 475, 480, 442, 402,
+                25, 2, 12, 32, 2, 0, 10, 20, 0, 25
+    ]
+
     k_big = int(os.environ.get("K_BIG", "20"))
     k = int(os.environ.get("K", "10"))
-    sleep = int(os.environ.get("SLEEP", "1")) #it will be automatically adjusted to 10s as soon the simulation starts 
+    sleep = int(os.environ.get("SLEEP", "10")) #it will be automatically adjusted to 10s as soon the simulation starts 
 
     config = Configurator(base, scale_config, microservices_mcl, microservices_mf, k_big)
     scaler = SysScaler(config, 60.0)
-    guard = Guard(scaler, k_big, k, sleep)
+    guard = Guard(scaler, predictions, k_big, k, sleep)
     guard.start()
