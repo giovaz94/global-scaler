791b5c9779cfff960812371399353c70359ee673
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-29 23:54:36 +0200

    log of simulation

--- src/components/guard.py
+++ src/components/guard.py
@@ -4,6 +4,8 @@ import threading
 from components.sys_scaler import SysScaler
 from components.mixer import Mixer
 from prometheus_api_client import PrometheusConnect
+import numpy as np
+import requests
 
 
 class Guard:
@@ -54,6 +56,16 @@ class Guard:
         """
         return inbound_workload - (current_mcl - self.k_big) > self.k or \
             (current_mcl - self.k_big) - inbound_workload > self.k
+    
+    def _execute_prometheus_query(self, query: str):
+        """
+        Execute a query to the Prometheus server.
+        """
+        try:
+            data = self.prometheus_instance.custom_query(query)
+            return float(data[0]['value'][1])
+        except (requests.exceptions.RequestException, KeyError, IndexError) as e:
+            pass
 
     def guard(self) -> None:
         """
@@ -61,13 +73,13 @@ class Guard:
         Check the conditions of the system and eventually scale it.
         """
         print("Monitoring the system...")
-        res = self.prometheus_instance.custom_query("sum(http_requests_total_parser)")
-        init_val = float(res[0]['value'][1])
+        init_val = self._execute_prometheus_query("sum(http_requests_total_parser)")
         sl = 1
         iter = 0
         last_pred_conf = []
         current_mcl = self.scaler.get_mcl()
         pred_workload = 0
+        config = self.scaler.get_current_config()
 
         if self.proactiveness:
             pred_workload = sum(self.predictions[iter-self.sleep:])/self.sleep
@@ -77,9 +89,12 @@ class Guard:
         while self.running:
             start = time.time()
         
-            print("Checking the system...", flush=True)
-            res = self.prometheus_instance.custom_query("sum(http_requests_total_parser)")
-            tot = float(res[0]['value'][1])    
+            tot = self._execute_prometheus_query("sum(http_requests_total_parser)")
+            completed = self._execute_prometheus_query("sum(increase(http_requests_total_global[10s]))")
+            latency = self._execute_prometheus_query("sum(increase(http_requests_total_time[10s]))")
+            avg_lat = latency/(completed if completed > 0 else 1)
+            toPrint = str(iter) + " " + str(avg_lat)
+              
 
             #reactivity
             measured_workload = (tot-init_val)/self.sleep
@@ -90,16 +105,18 @@ class Guard:
                 diff = iter-self.sleep
                 pred_workload = sum(self.predictions[diff if diff > 0 else 0:iter])/self.sleep
                 target_workload = pred_workload
+                toPrint += " next: " + str(pred_workload)
             
             #proactivity + reactivity:
             if iter > 0 and self.proactive_reactive:
                 measured_conf = self.scaler.calculate_configuration(measured_workload + self.k_big)
                 target_workload = self.mixer.mix(measured_workload, pred_workload, last_pred_conf, measured_conf)
                 last_pred_conf = self.scaler.calculate_configuration(pred_workload + self.k_big)
-            
+                toPrint += " mixed: " + str(target_workload)
+            toPrint += " measured: " + str(measured_workload) + " comp: " + str(completed) + " supp: " + str(current_mcl) + " inst: " + str(np.sum(config))
             config = self.scaler.get_current_config()
-            #TODO: CREATE A LOG FUNCTION AS THE ONE WE DID IN THE ABS SIMULATION!!!!
-            print(f"Target workload: {target_workload}, MCL: {current_mcl}, CONFIG: {config}", flush=True)
+            print(toPrint)
+
             if iter > 0 and self.should_scale(target_workload, current_mcl):
                 target_conf = self.scaler.calculate_configuration(target_workload + self.k_big)
                 current_mcl, _ = self.scaler.process_request(target_conf)    
